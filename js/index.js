63b7aaca959aa20119f5927705eaf3e89b3a3b67
ximenablard <87784183+ximenablard@users.noreply.github.com>
2024-10-20 23:21:55 -0600

    API: current temp and city
    Once the user searches for the city it will display the current temperature and the city name

--- js/index.js
+++ js/index.js
@@ -1,10 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#current-temp");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "2d4f050t38oe3346d712149ffb544a18";
+
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiURL).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  console.log(searchInput.value);
 
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
