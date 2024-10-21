9483e8d24cf5a1296019610ad39e046d9d3d357a
ximenablard <87784183+ximenablard@users.noreply.github.com>
2024-10-20 23:52:02 -0600

    Date and Time
    The app now shows the correct day and time of the city

--- js/index.js
+++ js/index.js
@@ -13,6 +13,33 @@ function refreshWeather(response) {
 
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
+
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
