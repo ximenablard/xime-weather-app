61bd78a12c18c7d5450a2a16654998c660111b12
ximenablard <87784183+ximenablard@users.noreply.github.com>
2024-10-29 04:44:52 -0600

    Inject the forecast HTML using JS

--- js/index.js
+++ js/index.js
@@ -63,8 +63,31 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHTML = "";
+
+  days.forEach(function (day) {
+    forecastHTML += `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">🌈</div>
+    <div class="weather-forecast-temperatures">
+      <div class="highest-temp">15°</div>
+      <div class="lowest-temp">9°</div>
+    </div>
+  </div>
+`;
+  });
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Puntarenas");
+displayForecast();
